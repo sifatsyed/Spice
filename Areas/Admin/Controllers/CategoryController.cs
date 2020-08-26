a84396b2528318a16b9d8ffc7d1e2415ee368fec
sifat <sifat@DESKTOP-AJMMED2>
2020-08-26 17:45:31 -0500

    Added background image and modified css for this purpose

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        //We're going to do dependency injection here
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+           return View(await _db.Category.ToListAsync());
+        }
+    }
+}
