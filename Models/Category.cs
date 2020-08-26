886d6ae2e37d5aaf792a8a4e1ed1993cfa0a5530
sifat <sifat@DESKTOP-AJMMED2>
2020-08-26 16:51:28 -0500

    Initial Commit

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class Category
+    {
+        //Specifying that it is a primary key
+        [Key]
+        public int Id { get; set; }
+
+        //Specifying that it is a required attribute
+        [Required]
+        public string Name { get; set; }
+    }
+}
